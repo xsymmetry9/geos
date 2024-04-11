f9dfa57515e80528847a1df8ea78963bcaa295d9
xsymmetry9 <glswim@gmail.com>
2024-04-11 20:40:52 +0800

    Fixed style

--- src/components/PersonalInformation.jsx
+++ src/components/PersonalInformation.jsx
@@ -16,54 +16,55 @@ const PersonalInformation = ({props}) =>{
                 <div className='d-grid'>
                     <div class="row">
                         <div className='input-container'>
-                            <label>Name</label>
-                            <input
-                                id="name"
-                                name="name"
-                                type="text"
-                                className='form-input'
-                                value = {data.name}
-                                onChange={(e) => handleChange(e)}
-                                placeholder='Student Name'>
-                            </input>
+                            <label><span>Name</span>
+                                <input
+                                    id="name"
+                                    name="name"
+                                    type="text"
+                                    className='form-input'
+                                    value = {data.name}
+                                    onChange={(e) => handleChange(e)}
+                                    placeholder='Student Name'>
+                                </input>
+                            </label>
                         </div>
                         <div className="input-container">
-                            <label>Textbook</label>
-                            <input
-                                id="course"
-                                name="course"
-                                type="text"
-                                className='form-input'
-                                value={data.course}
-                                onChange={(e) => handleChange(e)}>  
-                            </input>
+                            <label><span>Textbook</span>
+                                <input
+                                    id="course"
+                                    name="course"
+                                    type="text"
+                                    className='form-input'
+                                    value={data.course}
+                                    onChange={(e) => handleChange(e)}>  
+                                </input></label>
                         </div>
                     </div>
 
                     <div className="d-grid">
                         <div className='row'>
                             <div className='input-container'>
-                                <label>Attend￼
-￼Nextance</label>
-                                <input
-                                    id="attendance"
-                                    name="attendance"
-                                    type="number"
-                                    className='form-input'
-                                    value={data.attendance}
-                                    onChange={(e) => handleChange(e)}>
-                                </input>    
+                                    <label><span>Attendance</span>
+                                    <input
+                                        id="attendance"
+                                        name="attendance"
+                                        type="number"
+                                        className='form-input'
+                                        value={data.attendance}
+                                        onChange={(e) => handleChange(e)}>
+                                    </input>   
+                                </label>
                             </div>
                             <div className='input-container'/>
-                                <label>Total Lessons</label>
-                                <input
-                                    id="total-lesson"
-                                    className='form-input'
-                                    name="total-lessons"
-                                    type="number"
-                                    value={data.totalLessons}
-                                    onChange={(e) => handleChange(e)}>
-                                </input>
+                                <label><span>Total Lessons</span>
+                                    <input
+                                        id="total-lesson"
+                                        className='form-input'
+                                        name="total-lessons"
+                                        type="number"
+                                        value={data.totalLessons}
+                                        onChange={(e) => handleChange(e)}>
+                                    </input></label>
                             </div>
                         </div>
                     </div>
