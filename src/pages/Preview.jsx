6febba1d563af4b1af660ddde13306bc53f169bc
xsymmetry9 <glswim@gmail.com>
2024-04-27 18:01:44 +0800

    added more code

--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -7,8 +7,8 @@ const Preview = (data) =>{
         return(
             <>
             <div className='two-columns-container'>
-                <p>Student Name</p>
-                <p className='justify-end'>Gary</p>
+                <p className='personalInformation-title'>Student Name</p>
+                <p className='personalInformation-title justify-end'>Gary</p>
             </div>
             </>
         )
@@ -17,8 +17,8 @@ const Preview = (data) =>{
         return(
             <>
                 <div className='two-columns-container'>
-                    <p>SGL</p>
-                    <p className='justify-end'>EF4</p>
+                    <p className='personalInformation-title'>SGL</p>
+                    <p className='personalInformation-description justify-end'>EF4</p>
                 </div>
             </>
         )
@@ -27,8 +27,8 @@ const Preview = (data) =>{
         return(
             <>
             <div className='two-columns-container'>
-                <p>Date</p>
-                <p className='justify-end'>2024/03/26</p>
+                <p className='personalInformation-description'>Date</p>
+                <p className='personalInformation-description justify-end'>2024/03/26</p>
             </div>
         
             </>
@@ -39,8 +39,8 @@ const Preview = (data) =>{
         return(
             <>
                 <div className='two-columns-container'>
-                    <p>Attendance</p>
-                    <p className='justify-end'>14 times</p>
+                    <p className='personalInformation-description'>Attendance</p>
+                    <p className='personalInformation-description justify-end'>14 times</p>
                 </div>
       
             </>
@@ -49,16 +49,16 @@ const Preview = (data) =>{
     const TotalLessons = () =>{
         return(
             <div className='two-columns-container'>
-                <p>Total Lessons</p>
-                <p className='justify-end'>19 times</p>
+                <p className='personalInformation-description'>Total Lessons</p>
+                <p className='personalInformation-description justify-end'>19 times</p>
             </div>
         )
     }
     const RateOfAttendance = () =>{
         return(
             <div className='two-columns-container'>
-                <p>% of Attendance</p>
-                <p className='justify-end'>84%</p>
+                <p className='personalInformation-description'>% of Attendance</p>
+                <p className='personalInformation-description justify-end'>84%</p>
             </div>
         )
     }
@@ -75,9 +75,57 @@ const Preview = (data) =>{
                         {labelObj.map((item, index) => <div key={index} className='d-flex'><p>{item.level}.</p><p>{item.name}</p></div>)}
                     </div>)
             }
+        const Table = () =>{
+            return(
+                <div className='table'>
+                    <div className="row">
+                        <div className="col"><p></p></div>
+                        <div className="col"><p className='table-header'>Vocabulary</p></div>
+                        <div className="col"><p className='table-header'>Grammar</p></div>
+                        <div className="col"><p className='table-header'>Pronunciation</p></div>
+                        <div className="col"><p className='table-header'>Listening</p></div>
+                        <div className="col"><p className='table-header'>Conversation</p></div>
+                        <div className="col"><p className='table-header'>Total</p>
+                        </div><div className="col"><p className='table-header'>Average</p></div>
+                    </div>
+                    <div className="row">
+                        <div className='col'><p className='title-table'>Initial Level</p></div>
+                        <div className='col'><p id="initial-vocabulary">8</p></div>
+                        <div className='col'><p id="initial-grammar">8</p></div>
+                        <div className='col'><p id="initial-pronunciation">8</p></div>
+                        <div className='col'><p id="initial-listening">8</p></div>
+                        <div className='col'><p id="initial-conversation">8</p></div>
+                        <div className='col'><p id="initial-total">8</p></div>
+                        <div className='col'><p id="initial-average">8</p></div>
+                    </div>
+                    <div className="row">
+                        <div className='col'><p className='title-table'>Target</p></div>
+                        <div className='col'><p id="target-vocabulary">8</p></div>
+                        <div className='col'><p id="target-grammar">8</p></div>
+                        <div className='col'><p id="target-pronunciation">8</p></div>
+                        <div className='col'><p id="target-listening">8</p></div>
+                        <div className='col'><p id="target-conversation">8</p></div>
+                        <div className='col'><p id="target-total">8</p></div>
+                        <div className='col'><p id="target-average">8</p></div>
+                    </div>
+                    <div className="row">
+                        <div className='col'><p className='title-table'>Initial Level</p></div>
+                        <div className='col'><p id="final-vocabulary">8</p></div>
+                        <div className='col'><p id="final-grammar">8</p></div>
+                        <div className='col'><p id="final-pronunciation">8</p></div>
+                        <div className='col'><p id="final-listening">8</p></div>
+                        <div className='col'><p id="final-conversation">8</p></div>
+                        <div className='col'><p id="final-total">8</p></div>
+                        <div className='col'><p id="final-average">8</p></div>
+                    </div>
+
+                </div>
+            )
+        }
         return(
             <>
                 <Label />
+                <Table />
             </>
 
         )
