bda3777a5cde99e925041ffe62a15c1ac1feff07
xsymmetry9 <glswim@gmail.com>
2024-08-12 17:31:04 +0800

    Added papersize

--- src/components/PlotPage.jsx
+++ src/components/PlotPage.jsx
@@ -165,7 +165,7 @@ const Preview = (data) =>{
     }
     return(
         <>
-            <div className='print-window'>
+            <div className='print-window papersize'>
                 <div className='title-container'>
                     <div className='img-container'>
                         <RenderLogo style ="logoName" description="logo"/>
