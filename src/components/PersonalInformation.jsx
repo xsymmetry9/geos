965e2a326db91de66963c9bec669b965ada6aed7
xsymmetry9 <glswim@gmail.com>
2024-09-21 15:22:55 +0800

    added option values

--- src/components/PersonalInformation.jsx
+++ src/components/PersonalInformation.jsx
@@ -7,33 +7,44 @@ const PersonalInformation = ({props, handleData}) =>{
         <>
             <Heading name="heading-2" styleName='form-title' titleName="Personal Information"/>
             <div className='wrapper-container'>
-                <label className='two-columns'>
-                            <span>Name</span>
-                            <RenderInput title="name" id="name" type="text" styleName="form-input" value={props.name} handler ={(e) => handleData(e)} placeHolder="Student Name"/>
+                <label htmlFor="name">
+                    <div className='two-columns'>
+                        <p>Name</p>
+                        <RenderInput title="name" id="name" type="text" styleName="form-input" value={props.name} handler ={(e) => handleData(e)} placeHolder="Student Name"/>
+                    </div>
                 </label>
-                <label className='two-columns'>
-                    <span>Course</span>
-                    <select id="options" value={props.course} onChange={() => console.log("clicked")}>
-                        <option value="">Select course</option>
-                        <option value="ONLINE">ONLINE</option>
-                        <option value="PL">PL</option>
-                        <option value="GL">GL</option>
-                        <option value="SGL">SGL</option>
-                        <option value="FLEX">FLEX</option>
-                    </select>
+                <label htmlFor="course">
+                    <div className="two-columns">
+                        <p>Course</p>
+                        <select id="course" name="course" value={props.course} onChange={handleData}>
+                            <option value="">Select course</option>
+                            <option value="ONLINE">ONLINE</option>
+                            <option value="PL">PL</option>
+                            <option value="GL">GL</option>
+                            <option value="SGL">SGL</option>
+                            <option value="FLEX">FLEX</option>
+                        </select>
                     {/* <RenderInput title="course" id="course" type="text" styleName="form-input" value={props.course} handler ={handleData} placeHolder="PL"/> */}
+                    </div>
+                    
                 </label>
-                <label className='two-columns'>
-                    <span>Textbook</span>
-                    <RenderInput title="textbook" id="textbook" type="text" styleName="form-input" value={props.textbook} handler ={handleData} placeHolder="EF1"/>                            
+                <label htmlFor= "textbook">
+                    <div className="two-columns">
+                        <p>Textbook</p>
+                        <RenderInput title="textbook" id="textbook" type="text" styleName="form-input" value={props.textbook} handler ={handleData} placeHolder="EF1"/>                            
+                    </div>
                 </label>
-                <label className='two-columns'>
-                    <span>Attendance</span>
-                    <RenderInput title="attendance" id="attendance" type="number" styleName="form-input" value={props.attendance} handler ={handleData} placeHolder="28"/>
+                <label htmlFor= "attendance">
+                    <div className="two-columns">
+                        <p>Attendance</p>
+                        <RenderInput title="attendance" id="attendance" type="number" styleName="form-input" value={props.attendance} handler ={handleData} placeHolder="28"/>
+                    </div>
                 </label>                             
-                <label className='two-columns'>
-                    <span>Total Lessons</span>
-                    <RenderInput title="totalLessons" id="total-lesson" type="number" styleName="form-input" value={props.totalLessons} handler ={handleData} placeHolder="30"/>                            
+                <label htmlFor="totalLessons">
+                    <div className='two-columns'>
+                        <p>Total Lessons</p>
+                        <RenderInput title="totalLessons" id="total-lesson" type="number" styleName="form-input" value={props.totalLessons} handler ={handleData} placeHolder="30"/>                          
+                    </div>  
                 </label>
             </div>
             
